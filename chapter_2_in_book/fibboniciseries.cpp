ce0900e7ea90c14140c8bc1908b2f78db2b5dee9
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-06-26 21:32:07 +0530

    did fibonnoci series

--- chapter_2_in_book/fibboniciseries.cpp
+++ chapter_2_in_book/fibboniciseries.cpp
@@ -1,1 +1,18 @@
-#inc
+#include<iostream>
+int fibonociSeries(int n){
+    if (n==1){
+    return 1;
+    }
+    if (n==0){
+    return 0;
+    }
+    if(n>1){
+        return (fibonociSeries(n-1)+fibonociSeries(n-2));
+    }
+}
+int main(){
+    int n;
+    std::cout<<"enter thr number of series you print: ";
+   std::  cin>>n;
+    std::cout<<"the fiboonoci of "<<n<<" is "<<fibonociSeries(n);
+}

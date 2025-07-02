4601d204e9c55d8771ebd801b2d32af315e5c2a9
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-02 10:33:58 +0530

    i have done hollow diamond pattern

--- pattern/18thpatternn.cpp
+++ pattern/18thpatternn.cpp
@@ -6,9 +6,51 @@ int main()
     cout << "enter no of rows: ";
     int n;
     cin >> n;
+     for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+
+
+    }
+    
     for (int i = 0; i < n; i++)
     {
-        
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 1; j < n-i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 1; j < n-i; j++)
+        {
+            cout<<"  ";
+        } 
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+
+
     }
+
     return 0;
 }

e30eb7f332815a9f26ab76677006f942bad929af
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-06 06:19:31 +0530

    completed maps

--- striversplaylist/stl/map.cpp
+++ striversplaylist/stl/map.cpp
@@ -6,6 +6,11 @@ int main()
 {
     map<string,int> student_marks;
     student_marks["sagar"] =90;
-    student_marks["princw"] = 92;
+    student_marks["prince"] = 92;
     student_marks["yash"]=91;
+    student_marks.insert({{"hardik",50},{"aditya s",40}});
+    map<string,int> :: iterator iter;
+    for(iter =student_marks.begin();iter!=student_marks.end();iter++){
+        cout<<iter->first<<" "<<iter->second<<endl;
+    }
 }

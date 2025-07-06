7bf805ff4da6a3d11334d38500e72189f47c082e
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-06 05:59:10 +0530

    created first map in cpp

---
+++ striversplaylist/stl/map.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+int main()
+{
+    map<string,int> student_marks;
+    student_marks["sagar"] =90;
+    student_marks["princw"] = 92;
+    student_marks["yash"]=91;
+}

33b1e1f9930b7ead163795ef940c0b3ef6d8094b
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-10 21:37:19 +0530

    mathametics make time complexity O(1)

--- striversplaylist/Recurrsion/Sum_of_First_N_Natural_Number_By_recurrsion.cpp
+++ striversplaylist/Recurrsion/Sum_of_First_N_Natural_Number_By_recurrsion.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int sum =0;
+// int sum =0;
 int sum_Of_first_N_Numbers(int n,int sum=0)
 { 
     if(n<1){
@@ -12,10 +12,15 @@ int sum_Of_first_N_Numbers(int n,int sum=0)
    sum += sum_Of_first_N_Numbers(n-1,sum)+n;
 
 }
+// second way
+int sumOfN(int n){
+    return (n*(n+1))/2;
+}
 int main()
 {   int n;
     cout<<"Enter the number: ";
     cin>>n;
    cout<<sum_Of_first_N_Numbers(n)<<endl;
+   cout<<sumOfN(n)<<endl;
         return 0;
 }

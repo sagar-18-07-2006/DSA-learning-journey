8b4f042af7489882c4034eb951c9665eebf857b5
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-10 21:44:19 +0530

    put an important point

--- striversplaylist/Recurrsion/FactorialOfN.cpp
+++ striversplaylist/Recurrsion/FactorialOfN.cpp
@@ -8,12 +8,13 @@ int factorial(int n)
     }
     else if (n == 1)
     {
-        return 1;
+        return 1;// it have time compleexity of O(n) and space c omplexity of O(n) but for loop will have space complexity of O(i and time complexity is same )
     }
     else{ 
         return (n*factorial(n-1));
     }
 }
+
 int main (){
     int n;
     cout<<"Enter the number: ";

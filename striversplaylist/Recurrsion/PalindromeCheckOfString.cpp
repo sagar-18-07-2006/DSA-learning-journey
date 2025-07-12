96f8cc0d3e223942cc0231bc0bf381048c33d90a
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-12 13:46:50 +0530

    recurrsion basic is done

--- striversplaylist/Recurrsion/PalindromeCheckOfString.cpp
+++ striversplaylist/Recurrsion/PalindromeCheckOfString.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
-#include <string>
 using namespace std;
-string reverseString(string str)
-{
-    int start = 0;
-    int end = str.length() - 1;
-    while (end > start)
-    {
-        swap(str[start], str[end]);
-        start++;
-        end--;
-    }
-    return str;
-}
-bool PalindromeCheck(string str)
-{
-    string reverseStr = reverseString(str);
-    if (str == reverseStr)
-    {
-        return true;
-    }
-    else
-    {
-       return false;
-    }
-}
-int main(){
-    string str;
-    getline(cin,str);
-   cout<<str <<" is "<<PalindromeCheck(str)<<" ";
+
+bool palindrome( string& s,int i=0){
     
+    // Base Condition
+    // If i exceeds half of the string means all the elements 
+    // are compared, we return true.
+    if(i>=s.length()/2) return true;
+    
+    // If the start is not equal to the end, not the palindrome.
+    if(s[i]!=s[s.length()-i-1]) return false;
+    
+    // If both characters are the same, increment i and check start+1 and end-1.
+    return palindrome(s,i+1);
+}
+
+int main() {
+	
+	// Example string.
+	string s ;
+     getline(cin,s);
+	cout<<palindrome(s);
+	cout<<endl;
+	return 0;
 }

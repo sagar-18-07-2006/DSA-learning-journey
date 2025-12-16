cc639c5ee7dfb13e513d03058aee32f36be3df94
Sagar Maheshwari <sagarmaheshwari650@gmail.com>
2025-12-16 20:54:19 +0530

    video 17 done

---
+++ java/while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("hi" + (i + 1));
+            i++;
+        } while (i < 5);
+    }
+}

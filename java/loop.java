88abbed4c0547ab18653c658da9c5860d3882cf5
Sagar Maheshwari <sagarmaheshwari650@gmail.com>
2025-12-16 20:41:39 +0530

    video 16 done

---
+++ java/loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main(String[] args) {
+        //repeat this 4 times 
+        //loop while ,do while ,for 
+        
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+    }
+}

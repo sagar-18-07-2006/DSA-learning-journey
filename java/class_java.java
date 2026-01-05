329f1d0dff48a2c5e7fdff0e6e1792c49005a2ed
Sagar Maheshwari <sagarmaheshwari650@gmail.com>
2026-01-05 21:22:24 +0530

    diid started AGSIN

--- java/class_java.java
+++ java/class_java.java
@@ -1,10 +1,14 @@
  class calc {
     int a ;
+    
     public int add(int n1,int n2)
     {   System.out.println("Added numbers");
         return n1+n2;
     }
-    
+    public int add(int n1,int n2, int n3)
+    {   System.out.println("Added numbers");
+        return n1+n2+n3g;
+    }
 }
 
 public class class_java {
